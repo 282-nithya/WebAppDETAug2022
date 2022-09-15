9dab4785c9729731270bdd40baa8aa79d2d46fbe
282-nithya <nithyasatrasala9@gmail.com>
2022-09-15 17:57:39 +0530

    modified

---
+++ LibApi/service/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using LibApi.Model;
+
+namespace LibApi.service
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+        Book Add(Book newBook);
+        Book GetById(Guid id);
+        void Remove(Guid id);
+    }
+}
